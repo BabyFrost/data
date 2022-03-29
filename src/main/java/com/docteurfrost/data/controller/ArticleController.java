6b97c2cd7b133e496c6f840aabbb9afc4b412ea8
Voufack Harold <voufackharold@gmail.com>
2022-03-29 17:23:08 +0100

    Fixed issue : Call "Optional#isPresent()" before accessing the value

--- src/main/java/com/docteurfrost/data/controller/ArticleController.java
+++ src/main/java/com/docteurfrost/data/controller/ArticleController.java
@@ -1,5 +1,7 @@
 package com.docteurfrost.data.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,16 +30,16 @@ public class ArticleController {
 	
 	
 	@GetMapping("/article/{categorie}")
-	@ResponseBody public Iterable<Article>
+	@ResponseBody Iterable<Article>
 	getArticlesByCategorie( @PathVariable("categorie") String nomCategorie ) {
-		Categorie categorie;
+		Optional<Categorie> categorie = categorieRepository.findById(nomCategorie);
 		  
-		try {
-			categorie = categorieRepository.findById(nomCategorie).get();
-		} finally {
-		} 
-
-		return articleRepository.findAllByCategorie(categorie);	  
+		if ( categorie.isPresent() ) {
+			return articleRepository.findAllByCategorie( categorie.get() );
+		} else {
+			return null;
+		}
+	  
 	}
 	 
 }
