22cacf11ef99360fb6ef20d12b8d645c76e1c8f0
Voufack Harold <voufackharold@gmail.com>
2022-07-13 07:54:30 +0100

    daily commit

--- src/main/java/com/docteurfrost/data/dto/interne/ArticleInterneDTO.java
+++ src/main/java/com/docteurfrost/data/dto/interne/ArticleInterneDTO.java
@@ -1,27 +1,48 @@
 package com.docteurfrost.data.dto.interne;
 
+import java.util.ArrayList;
+
 import javax.validation.constraints.NotNull;
 
 import com.docteurfrost.data.model.article.Article;
+import com.docteurfrost.data.model.categorie.OptionArticle;
 
 public class ArticleInterneDTO { 
 
 	
 	private String id;
+	private String nom;
 	@NotNull
 	private String marque;
 	private int prix;
 	private int prixAchat;
 	private String photo;
+	private StringBuilder options;
 
 	public ArticleInterneDTO() { }
 	
 	public ArticleInterneDTO( Article article) {
 		this.id = article.getId();
+		this.nom = article.getNom().toUpperCase();
 		this.prix = article.getPrix();
 		this.prixAchat = article.getPrixAchat();
 		this.marque = article.getMarque().getNom();
 		this.photo = article.getPhoto();
+		
+		options = new StringBuilder();
+		ArrayList<OptionArticle> optionsArticle = new ArrayList<>( article.getOptions() );
+		OptionArticle optionArticle;
+		for ( int i=0; i<optionsArticle.size(); i++) {
+			optionArticle = optionsArticle.get(i);
+			options.append( "{" );
+				options.append( "" + optionArticle.getOption().getNom() + ":" );
+				options.append( "" + optionArticle.getValeur() + "" );
+			options.append( "}," );
+		}
+		if ( options.length() > 0 ) {
+			options.deleteCharAt(options.length()-1);
+		}
+		
 	}
 
 	public String getId() {
@@ -30,6 +51,14 @@ public class ArticleInterneDTO {
 
 	public void setId(String id) {
 		this.id = id;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom.toUpperCase();
 	}
 
 	public int getPrix() {
@@ -62,6 +91,20 @@ public class ArticleInterneDTO {
 
 	public void setPhoto(String photo) {
 		this.photo = photo;
+	}
+	
+	public String getOptions() {
+		
+		if ( this.options == null ) {
+			return null;
+		} else {
+			return options.toString();	
+		}
+		
+	}
+
+	public void setOptions(String options) {
+		this.options = new StringBuilder(options);
 	}
 
 }
