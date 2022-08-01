11fb2c2b80653ffb07085f0bcfa2f700879b4eec
Voufack Harold <voufackharold@gmail.com>
2022-08-01 13:55:18 +0100

    daily commit

--- src/main/java/com/docteurfrost/data/service/VenteService.java
+++ src/main/java/com/docteurfrost/data/service/VenteService.java
@@ -1,6 +1,7 @@
 package com.docteurfrost.data.service;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
@@ -39,8 +40,17 @@ public class VenteService {
 	}
 	
 	public List<Vente> getAllVenteByIntervalDate( Date dateDebut, Date dateFin ) throws ResourceNotFoundException {	
-		List<Vente> ventes = new ArrayList<>();	
-		venteRepository.findByDateBetween(dateDebut, dateFin).forEach(ventes::add);	
+		List<Vente> ventes = new ArrayList<>();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.HOUR_OF_DAY,23);
+		cal.set(Calendar.MINUTE,59);
+		cal.set(Calendar.SECOND,59);
+		cal.set(Calendar.MILLISECOND,59);
+		dateFin = cal.getTime();
+		
+		venteRepository.findByDateBetween(dateDebut, dateFin).forEach(ventes::add);		
+			
 		return ventes;
 	}
 	
